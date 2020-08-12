b4d1025d1064652cd27f0ff903355fc62e350d56
YusukeMancity <yusuke1548@gmail.com>
2020-08-12 23:28:44 +0900

    usersテーブルの作成、slim化

---
+++ db/migrate/20200626124022_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false #メールアドレス
+      t.string :family_name, null: false #姓
+      t.string :given_name, null: false #名
+      t.string :password_digest #パスワード
+
+      t.timestamps
+    end
+  end
+end
